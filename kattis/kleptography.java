8ff75af9a5315b25da19f1034c818491bd2d01e3
laurenmacpherson <lauren.macpherson@sjsu.edu>
2021-06-09 21:07:25 -0700

    kleptography

---
+++ kattis/kleptography.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Kleptography {
+    public static void main(String a[]){
+        Scanner in = new Scanner (System.in); 
+        int n, m; 
+        String a, b; 
+
+        n = in.nextInt(); 
+        m = in.nextInt(); 
+        
+        a = in.nextLine(); 
+        b = in.nextLine(); 
+
+        a = Integer.toString(m-n) + a; 
+    }
+}
